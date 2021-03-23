edae55ddc14402d6c568d41330be8ca247d2db4d
nyovelt <qinfr@shanghaitech.edu.cn>
2021-03-23 09:54:07 +0000

    update

--- apt/update_apt_source.sh
+++ apt/update_apt_source.sh
@@ -1,13 +1,13 @@
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
 SYS_VERSION=$(lsb_release -c | grep -o "\s.*")
 cat >> /etc/apt/sources.list << EOF
-deb https://mirrors.geekpie.club/debian/ $SYS_VERSION main 
-deb-src https://mirrors.geekpie.club/debian/ $SYS_VERSION main 
+deb https://mirrors.sjtug.sjtu.edu.cn/debian/ buster main 
+deb-src https://mirrors.sjtug.sjtu.edu.cn/debian/ buster main 
 
-deb http://security.debian.org/debian-security  $SYS_VERSION/updates main 
-deb-src http://security.debian.org/debian-security $SYS_VERSION/updates main 
+deb http://security.debian.org/debian-security  buster/updates main 
+deb-src http://security.debian.org/debian-security buster/updates main 
 
-deb https://mirrors.geekpie.club/debian/ $SYS_VERSION-updates main 
-deb-src https://mirrors.geekpie.club/debian/ $SYS_VERSION-updates main 
+deb https://mirrors.sjtug.sjtu.edu.cn/debian/ buster-updates main 
+deb-src https://mirrors.sjtug.sjtu.edu.cn/debian/ buster-updates main 
 EOF
 apt update
